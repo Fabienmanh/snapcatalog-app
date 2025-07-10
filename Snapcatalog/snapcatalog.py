debd1230ed276660355febb2948d19df807aeb37
Fabienmanh <fabienmanhes@gmail.com>
2025-07-10 16:16:23 +0200

    Update snapcatalog.py

--- Snapcatalog/snapcatalog.py
+++ Snapcatalog/snapcatalog.py
@@ -53,9 +53,11 @@ TEMPLATES = [
 PALETTE_1 = ["#e53935", "#c62828", "#8e24aa", "#43a047", "#d4e157", "#81c784"]
 PALETTE_2 = ["#1976d2", "#1976d2", "#64b5f6", "#90caf9", "#0097a7", "#263238"]
 DIAGRAMMES = [
-    {"label": "Camembert", "file": "images/chart1.png"},
-    {"label": "Graphique 1", "file": "images/chart2.png"},
-    {"label": "Graphique 2", "file": "images/chart3.png"},
+DIAGRAMMES = [
+    {"label": "Camembert", "file": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Pie-chart-example.png"},
+    {"label": "Graphique 1", "file": "https://upload.wikimedia.org/wikipedia/commons/7/77/Bar_chart_example.png"},
+    {"label": "Graphique 2", "file": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Line_chart_example.png"},
+]
 ]
 PEXELS_API_KEY = "TA_CLE_PEXELS_ICI"
 
